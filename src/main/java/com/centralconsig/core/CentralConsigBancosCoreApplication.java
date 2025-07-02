9ccf220f6eab5b14407200e9b06b8a37ee0a18c3
Bossa Web Solutions <gvfbossa@hotmail.com>
2025-07-02 12:12:29 -0300

    Estrutura inicial SPring 3.5.2 e Java 21

---
+++ src/main/java/com/centralconsig/core/CentralConsigBancosCoreApplication.java
@@ -0,0 +1,13 @@
+package com.centralconsig.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CentralConsigBancosCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CentralConsigBancosCoreApplication.class, args);
+	}
+
+}
